1a840570b01ac2ba109175cd3578ee99fa904a04
Emma Sandke <emmasandke@MacBook-Pro-88.local>
2020-10-11 13:15:50 -0400

    added hello string and object end points

---
+++ src/main/java/com/example/cs3200fa2020serverjavaemmasandke/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.example.cs3200fa2020serverjavaemmasandke.hello;
+
+public class Hello {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
